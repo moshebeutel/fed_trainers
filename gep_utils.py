fix basis gradients bug in public

gep_utils.py
@@ -1,5 +1,4 @@
 from typing import Optional, Tuple
-
 import numpy as np
 import torch
 from sklearn.decomposition import PCA
@@ -61,6 +60,9 @@ def get_bases(pub_grad, num_bases) -> Tuple[int, torch.Tensor]:
     num_bases = min(num_bases, min(num_p, num_k))
 
     pca = torch.pca_lowrank(pub_grad, q=num_bases, niter=2)
+    # error_rate = check_approx_error(pca[-1], pub_grad)
+
+    # print(f'\n\t\t\t\t\t\t\t\tnum_bases {num_bases}\tPCA error: {error_rate}')
 
     return num_bases, pca[-1]
 
@@ -85,17 +87,18 @@ def compute_subspace(basis_gradients: torch.Tensor, num_basis_elements: int) -> 
 @torch.no_grad()
 def add_new_gradients_to_history(new_gradients: torch.Tensor, basis_gradients: Optional[torch.Tensor],
                                  gradients_history_size: int) -> Tensor:
+    # print(f'\n\t\t\t\t\t\t\t\t1 - basis gradients shape {basis_gradients.shape if basis_gradients is not None else None}')
+
     basis_gradients = torch.cat((basis_gradients, new_gradients), dim=0) \
         if basis_gradients is not None \
         else new_gradients
+    # print(f'\n\t\t\t\t\t\t\t\t2 - basis gradients shape {basis_gradients.shape}')
+
     basis_gradients = basis_gradients[-gradients_history_size:] \
         if gradients_history_size < basis_gradients.shape[0] \
         else basis_gradients
+
+    # print(f'\n\t\t\t\t\t\t\t\t3 - basis gradients shape {basis_gradients.shape}')
+
     return basis_gradients
 
-
-@torch.no_grad()
-def update_subspace(basis_gradients, grads_flattened, history_size, basis_size) -> torch.Tensor:
-    basis_gradients = add_new_gradients_to_history(grads_flattened, basis_gradients, history_size)
-    pca = compute_subspace(basis_gradients, basis_size)
-    return pca

trainer_gep_public_no_gp.py
@@ -4,9 +4,9 @@ from collections import OrderedDict
 from typing import Optional
 import numpy as np
 import torch
-import wandb
 from tqdm import trange
-from gep_utils import update_subspace, embed_grad, project_back_embedding
+from gep_utils import embed_grad, project_back_embedding, add_new_gradients_to_history, \
+    compute_subspace
 from model import get_model
 from utils import get_clients, get_device, local_train, flatten_tensor, eval_model, update_frame, log2wandb, \
     load_aggregated_grads_to_global_net
@@ -65,8 +65,11 @@ def train(args, dataloaders):
 
         public_grads_list = [torch.stack(public_grads[n]) for n, p in net.named_parameters()]
 
-        public_grads_flattened = flatten_tensor(public_grads_list)
-        pca = update_subspace(basis_gradients, public_grads_flattened, args.gradients_history_size, args.basis_size)
+        public_grads_flat = flatten_tensor(public_grads_list)
+
+        basis_gradients = add_new_gradients_to_history(public_grads_flat, basis_gradients, args.gradients_history_size)
+
+        pca = compute_subspace(basis_gradients, args.basis_size)
 
         # *** End of public subspace update
 